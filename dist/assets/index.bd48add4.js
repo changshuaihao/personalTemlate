a312161edc5ce6191f7b1c55766d3d52d6793c7f
crp <2276409801@qq.com>
2026-09-21 03:14:12 +0800

    fix: .gitignore去除dist,vite.config去除base,添加dist、node_modules

---
+++ dist/assets/index.bd48add4.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.2ea590c6.js";const a=s=>e.get(`auth/sendSMSCode?phone=${s}`),o=s=>e.post("auth/checkCodeAndGetUsers",s),p=s=>e.post("auth/updatePassword",s);export{o as c,a as s,p as u};
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/personalTemlate/', //  资源路径添加上github仓库名
+  // base: '/personalTemlate/', //  资源路径添加上github仓库名
   server: {
     proxy: {
       '/api': {
